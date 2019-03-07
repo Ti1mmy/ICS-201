977a816d028db318312178b99e59e114d6fa8002
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2019-03-07 10:13:29 -0500

    Add files via upload

---
+++ Loops 1/Loops 1 - 2.py
@@ -0,0 +1,6 @@
+# Create a list of numbers. Create a program that will iterate through the list and print only the numbers greater than 10.
+
+numbers = [1, 3, 4, 5, 6, 7, 10, 20, 3423, 40, 43, 80, 45]
+for i in numbers:
+    if i > 10:
+        print(i)
