f1e03a75b846cb2fb4fd68e899ee88ac20546df6
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2019-03-28 19:58:53 -0400

    Delete Loops 1 - 4.py

--- classwork-master/Loops 1/Loops 1 - 4.py
+++
@@ -1,5 +0,0 @@
-# Ask the user for their name. Create a program that will loop OVER A RANGE and print each character of their name on seperate lines. You will access each character by using the character's index value. E.g., char = name[0]
-
-name = input('What is your name? ')
-for i in range(len(name)):
-    print(name[i])
