b194a72d97000ba0e76aa5ddc76b670e660961ee
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2019-03-28 19:59:54 -0400

    Delete Exercise 34.py

--- classwork-master/TPW If Statements/Exercise 34.py
+++
@@ -1,7 +0,0 @@
-print('Even or Odd?')
-print('------------')
-n = int(input('Please type in an integer: '))
-if n % 2 == 0:
-    print(f'The number {n} is even.')
-else:
-    print(f'The number {n} is odd.')
