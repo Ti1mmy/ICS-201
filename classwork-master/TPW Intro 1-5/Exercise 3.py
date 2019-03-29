05a6db8317d327f3a3f240f5fe68dbb8613b48f4
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2019-03-28 20:00:48 -0400

    Delete Exercise 3.py

--- classwork-master/TPW Intro 1-5/Exercise 3.py
+++
@@ -1,8 +0,0 @@
-print('Area of a Room')
-print('----------------')
-print()
-width = float(input('What is the width of the room? (m) '))
-length = float(input('What is the length of the room? (m) '))
-area = width * length
-print()
-print(f'The area of the room is {round(area, 2)} square meters.')
