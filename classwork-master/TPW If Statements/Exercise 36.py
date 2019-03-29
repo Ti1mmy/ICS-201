5adf4f540019fbe87b4b2fafec7a9e8d7c058ffb
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2019-03-28 20:00:02 -0400

    Delete Exercise 36.py

--- classwork-master/TPW If Statements/Exercise 36.py
+++
@@ -1,8 +0,0 @@
-letter = str(input('Please type in your letter: '))
-if letter in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
-    print(f'The letter {letter} is a vowel.')
-elif letter in ['y', 'Y']:
-    print(f'The letter {letter} is only sometimes a vowel and sometimes a consanant.')
-    print('For more information, visit: https://english.stackexchange.com/questions/10458/when-is-y-a-vowel')
-else:
-    print(f'The letter {letter} is a consanant.')
