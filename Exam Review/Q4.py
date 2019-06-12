1e33fc832e5e1e0553a7d20bd2e38ddb885f623d
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2019-06-12 12:55:50 -0400

    Q4?

--- Exam Review/Q4.py
+++ Exam Review/Q4.py
@@ -1,3 +1,11 @@
 """
-Create a loop to add the numbers from variable a to variable b. You can fill in their literal values.
+Create a loop to add the numbers from variable a to variable b.
+You can fill in their literal values.
 """
+
+a = [1, 2, 3, 4, 5]
+b = [5, 4, 3, 2, 1]
+sum = []
+for i in range(len(a)):
+    sum.append(a[i] + b[i])
+print(sum)
